5d743325a063195ddc3d2223417948207f5c0ded
LuisAntonyᵇʳᶻˡ <72362837+luizzitow123@users.noreply.github.com>
2021-06-24 23:18:03 +0000

    correção de bugs

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function Main() {
 
 	document.addEventListener('keydown', (event) => {
 		
-		if(event.key.toLowerCase() === "backspace" &&  typeof code[i] !== "undefined") {
+		if(event.key.toLowerCase() === "backspace" && i<0||  typeof code[i] !== "undefined") {
 			let retirar = code[i];
 			let arr = str.split("");
 			arr.pop();
